700ea4ab4786aa88fa4c1765bbce9e977d5a2fc6
xiaominghupan <463785757@qq.com>
2015-08-29 22:06:00 +0800

    fix sh/bat

--- src/non-packaged-resources/search.bat
+++ src/non-packaged-resources/search.bat
@@ -1,9 +1,9 @@
 @echo off
+SET PROJECT_HOME=.
+SET MAIN_CLASS=personal.xiaominghupan.com.ppt_search.PPTSearch
+SET LIB_OPTS=%PROJECT_HOME%;%PROJECT_HOME%/lib;%PROJECT_HOME%/lib/*;%PROJECT_HOME%/classes;
 if "%2"=="" (
-  echo "Usage : %0 [FILE|DIR] [PATTERN]"
+  	echo "Usage : %0 [FILE|DIR] [PATTERN]"
 ) else (
-	SET PROJECT_HOME=.
-	SET MAIN_CLASS=personal.xiaominghupan.com.ppt_search.PPTSearch
-	SET LIB_OPTS=%PROJECT_HOME%;%PROJECT_HOME%/lib;%PROJECT_HOME%/lib/*;%PROJECT_HOME%/classes;
-	"%JAVA_HOME%\bin\java" -cp %LIB_OPTS% %MAIN_CLASS% "%1" "%2"
+	"%JAVA_HOME%\bin\java" -cp %LIB_OPTS% %MAIN_CLASS% %1 %2
 )
--- src/non-packaged-resources/search.sh
+++ src/non-packaged-resources/search.sh
@@ -1,10 +1,9 @@
 #/usr/bin
+PROJECT_HOME=.
+MAIN_CLASS=personal.xiaominghupan.com.ppt_search.PPTSearch
+LIB_OPTS="$PROJECT_HOME:$PROJECT_HOME/lib:$PROJECT_HOME/lib/*:$PROJECT_HOME/classes"
 if [ $# != 2 ];then
 	echo "Usage : $0 [FILE|DIR] [PATTERN]"
 else
-	echo "Searching $1..."
-	PROJECT_HOME=.
-	MAIN_CLASS=personal.xiaominghupan.com.ppt_search.PPTSearch
-	LIB_OPTS="$PROJECT_HOME:$PROJECT_HOME/lib:$PROJECT_HOME/lib/*:$PROJECT_HOME/classes"
 	java -cp $LIB_OPTS $MAIN_CLASS $1 $2
 fi
